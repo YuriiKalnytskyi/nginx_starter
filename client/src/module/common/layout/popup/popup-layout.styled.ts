c63a2a19574e96163049fd214566a434c92a7801
WebDesignSun <contact@webdesignsun.com>
2023-11-18 18:11:00 +0200

    fix  PopupLayoutBottom

--- client/src/module/common/layout/popup/popup-layout.styled.ts
+++ client/src/module/common/layout/popup/popup-layout.styled.ts
@@ -23,19 +23,28 @@ export const Container = styled.div<{ width?: string; minWidth?: string }>`
 `;
 
 export const ContainerBottom = styled.div<{ styled?: any }>`
-  width: 60%;
-  max-width: 60rem;
-  height: 100%;
-  background-color: ${COLORS.white};
-  border-radius: ${SPACES.l};
+display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 3.5rem;
+  width: ${({ width }) => width ?? '21rem'};
+  min-width: ${({ minWidth }) => minWidth ?? '21rem'};
+  font-family: ${FONTS.FAMILIES.inter};
+  border-radius: 12px;
+  background: ${COLORS.white};
+  box-shadow: 0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03);
 
   position: relative;
 
-  z-index: ${INDEX.popup};
-
+  & button:nth-of-type(1) {
+    margin-bottom: ${SPACES.s};
+  }
+  
   @media screen and (max-width: ${MEDIA.tablet}) {
     width: 100%;
+    max-height: 90%;
     border-radius: ${SPACES.l} ${SPACES.l} 0 0;
+      padding: 1.5rem;
   }
 
   ${({ styled }) => styled};
--- client/src/module/example/example.tsx
+++ client/src/module/example/example.tsx
@@ -1,113 +1,123 @@
-import { Form, Formik } from 'formik';
-import React, { useState } from 'react';
+import {Form, Formik} from 'formik';
+import React, {useState} from 'react';
 
 import {
-  Button,
-  CheckBoxFormik,
-  Drawer,
-  Input,
-  InputMatchedWords,
-  InputMatchedWordsDynamic,
-  InputTextArea,
-  Portal,
-  Switch
+    Button,
+    CheckBoxFormik,
+    Drawer,
+    Input,
+    InputMatchedWords,
+    InputMatchedWordsDynamic,
+    InputTextArea,
+    Portal,
+    Switch
 } from '@/module/common/component';
-import { InputsConst } from '@/module/common/constants';
-import { DrawerLayout, PopupLayoutBottom } from '@/module/common/layout';
-import { DivCommon } from '@/module/common/styles';
-import { validationSchemaExample } from '@/module/example/validation/shema';
-import { SPACES } from '@/theme';
+import {InputsConst} from '@/module/common/constants';
+import {DrawerLayout, PopupLayout, PopupLayoutBottom} from '@/module/common/layout';
+import {DivCommon} from '@/module/common/styles';
+import {validationSchemaExample} from '@/module/example/validation/shema';
+import {SPACES} from '@/theme';
 
 export const Example = () => {
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const [openPopupInDrawer, setOpenPopupInDrawer] = useState(false);
-  const [openPopup, setOpenPopup] = useState(false);
+    const [openDrawer, setOpenDrawer] = useState(false);
+    const [openPopupInDrawer, setOpenPopupInDrawer] = useState(false);
+    const [openPopup, setOpenPopup] = useState(false);
 
-  const onToggle = () => {
-    setOpenDrawer((prev) => !prev);
-  };
+    const onToggle = () => {
+        setOpenDrawer((prev) => !prev);
+    };
 
-  const onTogglePopupInDrawer = () => {
-    setOpenPopupInDrawer((prev) => !prev);
-  };
-  const onTogglePopup = () => {
-    setOpenPopup((prev) => !prev);
-  };
+    const onTogglePopupInDrawer = () => {
+        setOpenPopupInDrawer((prev) => !prev);
+    };
+    const onTogglePopup = () => {
+        setOpenPopup((prev) => !prev);
+    };
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
 
-  return (
-    <DivCommon fd='row' gap={SPACES.xxxxl} padding={`${SPACES.xxxxl} ${SPACES.xxxxl}`}>
-      <DivCommon width='25%'>
-        <Formik
-          initialValues={{
-            last_name: '',
-            password: '',
-            email: '',
-            text: '',
-            add_category: [],
-            category: [],
-            is_remember: false
-          }}
-          onSubmit={onSubmit}
-          validationSchema={validationSchemaExample}
-        >
-          {({ values }) => (
-            <Form>
-              <DivCommon gap={SPACES.l}>
-                <Input {...InputsConst.last_name} />
-                <Input {...InputsConst.password} type='password' />
-                <Input {...InputsConst.email} placeholder='' inputType={2} />
-                <InputTextArea
-                  name='text'
-                  label={'text'}
-                  placeholder='text'
-                  rows={2}
-                  maxLength={1000}
-                />
-                <InputMatchedWordsDynamic
-                  name='add_category'
-                  label='Add Category'
-                  placeholder='Add Category'
-                />
-                <InputMatchedWords
-                  matchedWords={values.add_category}
-                  name='category'
-                  label='Choice Category'
-                  placeholder='Choice Category'
-                />
-                <CheckBoxFormik name='is_remember' label='Remember for 30 days' />
-                <Switch name='is_remember' label='Remember for 30 days' />
+    return (
+        <DivCommon fd='row' gap={SPACES.xxxxl} padding={`${SPACES.xxxxl} ${SPACES.xxxxl}`}>
+            <DivCommon width='25%'>
+                <Formik
+                    initialValues={{
+                        last_name: '',
+                        password: '',
+                        email: '',
+                        text: '',
+                        add_category: [],
+                        category: [],
+                        is_remember: false
+                    }}
+                    onSubmit={onSubmit}
+                    validationSchema={validationSchemaExample}
+                >
+                    {({values}) => (
+                        <Form>
+                            <DivCommon gap={SPACES.l}>
+                                <Input {...InputsConst.last_name} />
+                                <Input {...InputsConst.password} type='password'/>
+                                <Input {...InputsConst.email} placeholder='' inputType={2}/>
+                                <InputTextArea
+                                    name='text'
+                                    label={'text'}
+                                    placeholder='text'
+                                    rows={2}
+                                    maxLength={1000}
+                                />
+                                <InputMatchedWordsDynamic
+                                    name='add_category'
+                                    label='Add Category'
+                                    placeholder='Add Category'
+                                />
+                                <InputMatchedWords
+                                    matchedWords={values.add_category}
+                                    name='category'
+                                    label='Choice Category'
+                                    placeholder='Choice Category'
+                                />
+                                <CheckBoxFormik name='is_remember' label='Remember for 30 days'/>
+                                <Switch name='is_remember' label='Remember for 30 days'/>
 
-                <Button content='Submit' variant='primary' type='submit' />
-              </DivCommon>
-            </Form>
-          )}
-        </Formik>
-      </DivCommon>
+                                <Button content='Submit' variant='primary' type='submit'/>
+                            </DivCommon>
+                        </Form>
+                    )}
+                </Formik>
+            </DivCommon>
 
-      <DivCommon gap={SPACES.l} width='25%'>
-        <Button content='open drawer' variant='primary' onClick={onToggle} />
-        <Button content='open popup' variant='inverse' onClick={onTogglePopup} />
-        <Button content='test' variant='primary' disabled />
-        <Button content='test' variant='inverse' disabled />
-      </DivCommon>
+            <DivCommon gap={SPACES.l} width='25%'>
+                <Button content='open drawer' variant='primary' onClick={onToggle}/>
+                <Button content='open popup' variant='inverse' onClick={onTogglePopup}/>
+                <Button content='test' variant='primary' disabled/>
+                <Button content='test' variant='inverse' disabled/>
+            </DivCommon>
 
-      <Drawer onClose={onToggle} open={openDrawer}>
-        <DrawerLayout title={'Test'} onClose={onToggle}>
-          <Button content='open Popup' variant='primary' onClick={onTogglePopupInDrawer} />
-        </DrawerLayout>
-      </Drawer>
+            <Drawer onClose={onToggle} open={openDrawer}>
+                <DrawerLayout title={'Test'} onClose={onToggle}>
+                    <Button content='open Popup' variant='primary' onClick={onTogglePopupInDrawer}/>
+                    <Button mt={SPACES.l} content='open Popup Bottom' variant='primary' onClick={onTogglePopup}/>
+                </DrawerLayout>
+            </Drawer>
 
-      <Portal>
-        {openPopupInDrawer ? (
-          <PopupLayout onClose={onTogglePopupInDrawer}>dcdkckdc</PopupLayout>
-        ) : null}
+            {
+                openPopup ? (
+                    <Portal>
+                        <PopupLayoutBottom onClose={onTogglePopup}>PopupLayoutBottom</PopupLayoutBottom>
+                    </Portal>
+                ) : null
+            }
 
-        {openPopup ? <PopupLayoutBottom>dkcdc</PopupLayoutBottom> : null}
-      </Portal>
-    </DivCommon>
-  );
+            {/*<Portal>*/}
+            {/*    {openPopupInDrawer ? (*/}
+            {/*        <PopupLayout onClose={onTogglePopupInDrawer}>dcdkckdc</PopupLayout>*/}
+            {/*    ) : null}*/}
+
+            {/*    {openPopup ?*/}
+            {/*        <PopupLayoutBottom onClose={onTogglePopupInDrawer}>PopupLayoutBottom</PopupLayoutBottom> : null}*/}
+            {/*</Portal>*/}
+        </DivCommon>
+    );
 };
